3670f84c1eab8c7da2eedf3c75dacd821dc221f5
SabitCanTurunc <turuncsc@gmail.com>
2024-01-08 04:25:26 +0300

    statics and ejs

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express  from "express";
 
 const app = express()
-const port = 3000
+const port = 3000 
+
+// ejs template engine
+app.set("view engine", "ejs");
+
+
+//statics files middleware
+app.use(express.static('public'));
 
 app.get("/",(req,res) =>{
-    res.send("ındex sayfası ");
+    res.render("index");
+});
+
+app.get("/about",(req,res)=>{
+    res.render("about");
 });
 
 
