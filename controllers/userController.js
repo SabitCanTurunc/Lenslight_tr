9234bab6430c1b7fa56e3231b64dff1c5a00a0f1
SabitCanTurunc <turuncsc@gmail.com>
2024-01-31 00:32:51 +0300

    login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 import User from "../models/userModel.js";
+import bcrypt from  "bcrypt";
+
 
 const createUser = async (req, res) => {
 
@@ -16,6 +18,45 @@ const createUser = async (req, res) => {
         });
     }
 };
+const loginUser = async (req, res) => {
+
+    try {
+        const { username, password} = req.body;
+
+        console.log("req.body",req.body);
+
+        const user = await User.findOne({username});
+        let same= false;
+
+        if(user){
+            same= await bcrypt.compare(password, user.password);
+            console.log("same",same);
+
+            }else{
+                
+                return res.status(401).json({
+                    succeded: false,
+                    error: 'There is no such user',
+                });
+                }
+
+        if (same){
+            res.status(200).send("you are logged in");
+        }else{
+            res.status(401).json({
+                succeded: false,
+                error: "password are not mached",
+            });
+        }
+
+    } catch (error) {
+        console.error("Hata oluştu:", error.message);
+        res.status(500).json({
+            succeeded: false,
+            error: error.message,
+        });
+    }
+};
 
 
-export { createUser };
+export { createUser,loginUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,7 @@ import * as userController from "../controllers/userController.js";
 const router =express.Router();
 
 router.route("/register").post(userController.createUser);
+router.route("/login").post(userController.loginUser);
+
 
 export default router; 
