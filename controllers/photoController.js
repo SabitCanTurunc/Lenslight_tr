1f4373a70c6ca9969f4aa347f01d5bf99dd3f0a8
SabitCanTurunc <turuncsc@gmail.com>
2024-02-05 20:05:59 +0300

    user redirection from all photos

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -32,11 +32,13 @@ const createPhoto = async (req, res) => {
 
 const getAllPhotos = async (req, res) => {
   try {
-    const photos = await Photo.find({user:{ $ne: res.locals.user._id}});
+    const photos = res.locals.user 
+    ? await Photo.find({user:{ $ne: res.locals.user._id}})
+    : await Photo.find();
     res.status(200).render('photos', {
       photos,
       link: 'photos',
-    });
+    })
   } catch (error) {
     res.status(500).json({
       succeded: false,
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,14 +2,14 @@ import express from "express";
 import * as userController from "../controllers/userController.js";
 import * as authMiddleware from "../middlewares/authMiddleware.js";
 
-const router =express.Router();
+const router = express.Router();
 
 
 router.route("/register").post(userController.createUser);
 router.route("/login").post(userController.loginUser);
 router.route("/dashboard").get(authMiddleware.authenticateToken, userController.getDashboardPage);
-router.route("/").get(userController.getAllUsers);
-router.route("/:id").get(userController.getAUser);
+router.route("/").get(authMiddleware.authenticateToken, userController.getAllUsers);
+router.route("/:id").get(authMiddleware.authenticateToken, userController.getAUser);
 
 
 export default router; 
