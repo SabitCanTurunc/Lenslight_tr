2c412aac3cf05a2b9d0f393f4d7a0b10df68d8dc
SabitCanTurunc <turuncsc@gmail.com>
2024-02-07 01:10:55 +0300

    photo update

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -11,8 +11,6 @@ const createPhoto = async (req, res) => {
     }
   );
 
-  console.log('RESULT::', result);
-
   try {
     await Photo.create({
       name: req.body.name,
@@ -45,7 +43,7 @@ const getAllPhotos = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       succeded: false,
-      error:error.message,
+      error,
     });
   }
 };
@@ -60,7 +58,7 @@ const getAPhoto = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       succeded: false,
-      error:error.message,
+      error,
     });
   }
 };
@@ -72,15 +70,51 @@ const deletePhoto = async (req, res) => {
     const photoId = photo.image_id;
 
     await cloudinary.uploader.destroy(photoId);
-    await Photo.findOneAndDelete({ _id: req.params.id });
+    await Photo.findOneAndRemove({ _id: req.params.id });
 
     res.status(200).redirect('/users/dashboard');
   } catch (error) {
     res.status(500).json({
       succeded: false,
-      error:error.message,
+      error,
     });
   }
 };
 
-export { createPhoto, getAllPhotos, getAPhoto, deletePhoto };
+const updatePhoto = async (req, res) => {
+  try {
+    const photo = await Photo.findById(req.params.id);
+
+    if (req.files) {
+      const photoId = photo.image_id;
+      await cloudinary.uploader.destroy(photoId);
+
+      const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+          use_filename: true,
+          folder: 'lenslight_tr',
+        }
+      );
+
+      photo.url = result.secure_url;
+      photo.image_id = result.public_id;
+
+      fs.unlinkSync(req.files.image.tempFilePath);
+    }
+
+    photo.name = req.body.name;
+    photo.description = req.body.description;
+
+    photo.save();
+
+    res.status(200).redirect(`/photos/${req.params.id}`);
+  } catch (error) {
+    res.status(500).json({
+      succeded: false,
+      error,
+    });
+  }
+};
+
+export { createPhoto, getAllPhotos, getAPhoto, deletePhoto, updatePhoto };
