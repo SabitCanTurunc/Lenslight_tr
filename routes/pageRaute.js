605518f90f845cbcef4e0de0ab8b3c944cd7983b
SabitCanTurunc <turuncsc@gmail.com>
2024-02-29 03:13:24 +0300

    send mail

--- routes/pageRaute.js
+++ routes/pageRaute.js
@@ -1,12 +1,14 @@
-import express from "express";
-import * as pageController from "../controllers/pageController.js";
-import * as authMiddleware from "../middlewares/authMiddleware.js";
+import express from 'express';
+import * as pageController from '../controllers/pageController.js';
+
 const router = express.Router();
 
-router.route("/").get(pageController.getIndexPage);
-router.route("/about").get(pageController.getAboutPage);
-router.route("/login").get(pageController.getLoginPage);
-router.route("/register").get(pageController.getRegisterPage);
-router.route("/logout").get(pageController.getLogoutPage);
+router.route('/').get(pageController.getIndexPage);
+router.route('/about').get(pageController.getAboutPage);
+router.route('/register').get(pageController.getRegisterPage);
+router.route('/login').get(pageController.getLoginPage);
+router.route('/logout').get(pageController.getLogout);
+router.route('/contact').get(pageController.getContactPage);
+router.route('/contact').post(pageController.sendMail);
 
 export default router;
